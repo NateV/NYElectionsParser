86da5d167b49e681b17fdf7b831f800142e46bcd
NateV <nathan.c.vogel@gmail.com>
2014-03-18 09:40:08 -0400

    I did it! Lots of refactoring is needed, though.

--- lib/elections_parser.rb
+++ lib/elections_parser.rb
@@ -6,6 +6,8 @@ require 'nokogiri'
 
 class ElectionsParser
 
+  attr_accessor :xml, :full_list, :districts_list, :record_date, :election_date
+
   def initialize (*args)
     #debugger
     if args[0].empty? || args[0].length != 2
@@ -16,15 +18,163 @@ class ElectionsParser
 	end
   end# of #new
   
-  def read_file_to_xml
+  def file_to_xml
     file = File.open(@FILE_IN)
-    xml = Nokogiri::HTML(file, nil, 'UTF-8') do |config|
+    @xml= Nokogiri::HTML(file, nil, 'UTF-8') do |config|
       config.nonet
     end
     file.close
-    xml
-  end# of #read_file
+    @xml
+  end# of #file_to_xml
   
+  def xml_to_list
+    @full_list = []
+    
+    @xml.xpath("//text").each do |element|
+      @full_list << element.text
+    end
+    @full_list
+  end# of xml_to_list
+  
+  def clean_list (list)
+  #If there are any numbers formatted with commas,
+  # the method removes those commas, and finds, stores, and removes the date.
+    list.each do |item| 
+      if /([0-9]+)+,[0-9]+/.match item 
+        item.gsub!(",","")
+      elsif /dated/i.match item
+        @record_date= /^dated:\ ([a-z]*\ [0-9]*,\ [0-9]*)\ \(/i.match(item)[1]
+      elsif /election\ returns/i.match item
+        @election_date= /election\ returns\ (.*)$/i.match(item)[1]
+      end
+    end
+    
+    #And removes page numbers
+    list.reject {|item| ((/page/i.match item) || (/dated/i.match item) || (/election\ returns/i.match item))}
+      
+  end# of clean_commas_from_list
+  
+  def break_list_into_districts 
+    @districts_list = []
+    current_district = []
+    @full_list.each do |string|
+      if /^([0-9]*)(st|nd|rd|th)\ SENATE\ DISTRICT$/.match(string) then
+		@districts_list << current_district
+		current_district = []
+		current_district << string
+	  else 
+	    current_district << string
+	  end
+    end# of each block
+    if current_district!=nil then
+      @districts_list << current_district
+    end
+    
+    if @districts_list[0]==[] then
+      @districts_list.shift
+    end
+    @districts_list
+  end# of #break_list_into_districts
+  
+  def break_districts_into_rows
+    @districts_list.map! do |district|
+      ##i've got an array of strings, and need to break them up into separate arrays for each row.
+      rows = [] #rows will be an array of arrays. row[row_number][column]
+      
+      #The first 3 rows I will identify based on values I know they will have.
+      county_index = district.find_index("County")
+      subtotal_index = district.find_index("Subtotal")
+     
+      last_party_index = district.rindex {|x| /^[A-Z]{3}$/i.match x}
+      rows[0] = district[0..county_index-1]
+      rows[1] = district[county_index..subtotal_index+1]
+      rows[2] = district[subtotal_index+2..last_party_index]
+      
+      #Now I'll remove the values I've taken so far because I am done with them:
+      district = district.drop(rows[0].length + rows[1].length + rows[2].length)
+      
+      #Then I will pop off the recap values, because I don't need them. 
+      recap_index = district.find_index {|x| /recap/i.match x}
+      district.slice!(recap_index..district.length-1)
+      
+      #the rest of the rows will have the same length - the same length as row 1.
+      target_length= rows[1].length
+      target_number_elements = district.length/target_length# this should be an even number.
+      while district.length>0
+        rows << district.slice!(0, target_length)
+      end
+      rows
+    end# of districts_list.each block
+    
+    @districts_list
+  end# of break_districts_into_rows
+  
+  def align_rows_of_each_district
+    @districts_list.map! do |district|
+		 
+	  columns=district[3].length #all the rows need to be as long as this.
+	  
+	  #pad the first row
+	  while district[0].find_index("BVS") < district[1].find_index("Subtotal") 
+        district[0].insert(district[0].find_index("BVS"),"")
+      end
+      while district[0].length < district[1].length
+        district[0].push("")
+      end
+      
+      #pad the row with parties. 
+      district[2].unshift("")
+      while district[2].length<district[1].length
+        district[2].push("")
+      end
+
+      district
+    end# of each block.
+      
+  end# of align_rows_of_each_district
+  
+  def transform_district_tables_to_hash
+    @districts_list.map! do |district|
+      result = {event: "election",
+    			record_date: @record_date,
+    			election_date: @election_date}
+ 
+      result[:district] = /^([0-9]*)(st|nd|rd|th)\ (senate|assembly)/i.match(district[0][0])[1]
+      result[:chamber]= /^([0-9]*)(st|nd|rd|th)\ (senate|assembly)/i.match(district[0][0])[3]
+      
+      ##NOT RIGHT FOR MORE COMPLICATED DISTRICTS!!
+      # go through each column
+      number_of_columns = district[1].length
+      number_of_rows = district.length
+      
+      vote_hashes =[]
+      for column_index in 1..number_of_columns-1
+        vote_hash= {candidate: district[0][column_index] + " " + district[1][column_index],
+        				party: district[2][column_index]}
+        county_votes = []
+        for row_index in 3..number_of_rows-1
+          if /^part\ of/i.match(district[row_index][0])
+            county_votes << {county: district[row_index][0],
+            				 vote_count: district[row_index][column_index]}
+          elsif /total/i.match(district[row_index][0])
+            vote_hash[:total_votes]=district[row_index][column_index]
+          end
+        end# of loop through rows 3 to end
+        vote_hash[:county_votes]=county_votes
+        vote_hashes << vote_hash
+      end# of loop through columns
+
+      result[:votes] = vote_hashes
+      result
+    end# of map each block
+  end# of transform_district_tables_to_hash
+ 
+  def print_hashes_to_json 
+    File.write @FILE_OUT, JSON.pretty_generate(@districts_list)
+  end# of #print_hashes_to_json
+  
+  
+  #deprecated
   def districts(xml)
 	districtList=[]
 	currentDistrict=[]
@@ -47,6 +197,7 @@ class ElectionsParser
 	districtList
   end# of #districts
   
+  #deprecated
   def district_list_to_json(district)
     #get a list of the values for a sigle district's election, and return a 
     # formatted json object with the correct vote totals. 
@@ -119,14 +270,27 @@ class ElectionsParser
     result.to_json
   end# of #district_list_to_json
   
+
   def run!
-	xml = read_file_to_xml
-	districtList = districts(xml)
-	districtList_JSON = districtList.map do |district|
-	  district_list_to_json(district)
-	end 
-	#debugger
-	File.write @FILE_OUT, districtList_JSON 
+	@f = File.new("logs/log_" + Time.now.to_s + ".txt", "w+")
+	@f.write "Running app \n"
+	file_to_xml
+	@f.write "-file_to_xml \n"
+	xml_to_list
+	@f.write "-xml_to_list \n"
+	@full_list = clean_list(@full_list)
+	@f.write "-clean_list \n"
+	break_list_into_districts
+	@f.write "-break_list_into_districts \n"
+	break_districts_into_rows
+	@f.write "-break_districts_into_rows \n"
+	align_rows_of_each_district
+	@f.write "-align_rows_of_each_district \n"
+	transform_district_tables_to_hash
+	@f.write "-transform_rows_of_each_district\n "
+	print_hashes_to_json
+	@f.write "Completed. Successfully?"
+	@f.close
   end# of #run!
 
 end
--- spec/elections_parser_spec.rb
+++ spec/elections_parser_spec.rb
@@ -2,48 +2,180 @@ require 'spec_helper.rb'
 require 'elections_parser.rb'
 
 describe ElectionsParser do
-  before(:each) do 
+#   before(:each) do 
+#     @parser = ElectionsParser.new ["../NYLegislatureEvents/spec/fixtures/sample.xml", "output_test.txt"]
+#   	@parser.run!
+#   end
+#   
+#   it "evaluates simple tests." do
+#     5.should==5
+#   end# of simple tests
+# 
+#   it 'reads in a file and parses it into a nokogiri document.' do
+#     @parser.read_file_to_xml.class.should==Nokogiri::HTML::Document
+#   end
+#   
+#   it 'separates districts' do
+#     xml = @parser.read_file_to_xml
+#     @parser.districts(xml).length.should==2
+#   end
+# 
+#   it 'parses a single districts values into a json object.' do
+# 	correctJSON = {event: "election",
+# 				 	date: "Nov-6-2012",
+# 				 	chamber: "senate",
+# 				 	district: "1",
+# 				 	county: "Part of Suffolk",
+# 				 	votes: [ {candidate: "Bridget M. Fleming",
+# 				 				party: "DEM",
+# 				 				vote_count: "47041"},
+# 				 			 {candidate: "Kenneth P. LaValle",
+# 				 			  	party: "REP",
+# 				 			  	vote_count: "61309"}],
+# 				 	recap: [ {candidate: "Bridget M. Fleming",
+# 				 				vote_count: "51301"},
+# 				 			 {candidate: "Kenneth P. LaValle",
+# 				 			 	vote_count: "76006"}]}
+# 	xml = @parser.read_file_to_xml
+#     districtList = @parser.districts(xml)
+#     results= @parser.district_list_to_json(districtList[0])
+#     results = JSON.parse(results)
+#     results.class.should==Hash
+#     results["district"].should=="1"
+# 	results["chamber"].should=="SENATE"
+# 	results["votes"].class.should==Array
+#   end
+# 
+  before(:each) do
     @parser = ElectionsParser.new ["../NYLegislatureEvents/spec/fixtures/sample.xml", "output_test.txt"]
-  	@parser.run!
   end
+
+  specify {expect(@parser).to be_a ElectionsParser}
+
+  describe "#file_to_xml" do
+    it 'reads an xml file and returns a nokogiri document.' do
+      @parser.file_to_xml.class.should==Nokogiri::HTML::Document
+    end
+    
+    it 'stores the result in an instance variable' do
+      @parser.file_to_xml
+      @parser.xml.class.should==Nokogiri::HTML::Document
+    end
+  end# of #file_to_xml
+
+  describe "#xml_to_list" do
+    before(:each) do
+      @parser.file_to_xml
+    end
+    
+    it 'parses xml elements from a collection of <text> nodes into an array of the content of the nodes.' do
+      @parser.xml_to_list.class.should==Array
+    end
+    
+    it 'stores the result in an instance variable, full_list' do
+      @parser.xml_to_list
+      @parser.full_list.class.should==Array
+      @parser.full_list[5].class.should==String
+    end
+  end# of #xml_to_list
+
+  describe "clean_list" do
+    it 'cleans commas from only numbers in an array of strings' do
+      @parser.clean_list(["Some, text", "23,532", "424", "Page 9", "DATED: March 20, 2013 ( Kings* updated June 9, 2013)", "NYS Board of Elections Senate Election Returns Nov. 6, 2012"]).should==
+    								  ["Some, text", "23532", "424"]
+    end
+    
+    it 'finds the record date' do
+      @parser.clean_list(["nothing", "DATED: March 20, 2013 ( Kings* updated June 9, 2013)"])
+      @parser.record_date.should=="March 20, 2013"
+    end
+    
+    it 'finds the election date' do
+      @parser.clean_list(["nothing", "NYS Board of Elections Senate Election Returns Nov. 6, 2012"])
+	  @parser.election_date.should=="Nov. 6, 2012"
+    end
+  end# of #clean_commas_from_list
   
-  it "evaluates simple tests." do
-    5.should==5
-  end# of simple tests
-
-  it 'reads in a file and parses it into a nokogiri document.' do
-    @parser.read_file_to_xml.class.should==Nokogiri::HTML::Document
-  end
+  describe "#break_list_into_districts" do
+    before(:each) do
+      @parser.file_to_xml
+      @parser.xml_to_list
+      @parser.full_list= @parser.clean_list(@parser.full_list)
+      @parser.break_list_into_districts
+    end
+    
+    it 'breaks the list of elements into a list, each element of which is a list of the strings
+    	related to a particular district.' do
+    	expect(@parser.districts_list).to be_a Array
+    	expect(@parser.districts_list[0]).to be_a Array
+    	/[senate|assembly]/i.match(@parser.districts_list[0][0]).should be_true
+    end 
+  end# of #break_list_into_distrocts
   
-  it 'separates districts' do
-    xml = @parser.read_file_to_xml
-    @parser.districts(xml).length.should==2
-  end
-
-  it 'parses a single districts values into a json object.' do
-	correctJSON = {event: "election",
-				 	date: "Nov-6-2012",
-				 	chamber: "senate",
-				 	district: "1",
-				 	county: "Part of Suffolk",
-				 	votes: [ {candidate: "Bridget M. Fleming",
-				 				party: "DEM",
-				 				vote_count: "47041"},
-				 			 {candidate: "Kenneth P. LaValle",
-				 			  	party: "REP",
-				 			  	vote_count: "61309"}],
-				 	recap: [ {candidate: "Bridget M. Fleming",
-				 				vote_count: "51301"},
-				 			 {candidate: "Kenneth P. LaValle",
-				 			 	vote_count: "76006"}]}
-	xml = @parser.read_file_to_xml
-    districtList = @parser.districts(xml)
-    results= @parser.district_list_to_json(districtList[0])
-    results = JSON.parse(results)
-    results.class.should==Hash
-    results["district"].should=="1"
-	results["chamber"].should=="SENATE"
-	results["votes"].class.should==Array
-  end
-
+  describe "#break_districts_into_rows" do
+    before(:each) do
+      @parser.file_to_xml
+      @parser.xml_to_list
+      @parser.full_list= @parser.clean_list(@parser.full_list)
+      @parser.break_list_into_districts
+      @parser.break_districts_into_rows
+    end
+    
+    it 'breaks the array of elements for each district into arrays for each table row.' do
+      district1= @parser.districts_list[0]
+      dist_61 = @parser.districts_list[2]
+      expect(district1).to be_a Array
+      expect(dist_61).to be_a Array
+      expect(district1[0]).to be_a Array
+      expect(dist_61).to be_a Array
+      /senate/i.match(district1[0][0]).should_not be_nil
+      /part/i.match(district1[3][0]).should_not be_nil
+    end
+  end# of #break_districts_into_rows
+  
+  describe "#align_rows_of_each_district" do
+    before(:each) do
+      @parser.file_to_xml
+      @parser.xml_to_list
+      @parser.full_list= @parser.clean_list(@parser.full_list)
+      @parser.break_list_into_districts
+      @parser.break_districts_into_rows
+      @parser.align_rows_of_each_district
+    end
+    
+    it 'correctly aligns the list of strings according to the tables in the election results.' do
+      district1= @parser.districts_list[0]
+      dist_61 = @parser.districts_list[2]
+      (district1[0][1] + " " + district1[1][1]).should=="Bridget M. Fleming"
+      dist_61[2][1].should=="DEM"
+      
+      subtotal_index = dist_61[1].find_index("Subtotal")
+      dist_61[0][subtotal_index].should=="BVS"
+    end
+  end# of align_rows_of_one_district
+  
+  describe "#transform_district_tables_to_hash" do
+    before(:each) do
+      @parser.file_to_xml
+      @parser.xml_to_list
+      @parser.full_list= @parser.clean_list(@parser.full_list)
+      @parser.break_list_into_districts
+      @parser.break_districts_into_rows
+      @parser.align_rows_of_each_district
+      @parser.transform_district_tables_to_hash
+    end
+    
+    it "transforms an array of rows of election results into a hash" do
+      expect(@parser.districts_list[0]).to be_a Hash
+      @parser.districts_list[0][:district].should=="1"
+      @parser.districts_list[0][:chamber].should=="SENATE"
+      expect(@parser.districts_list[2][:votes]).to be_a Array
+    end
+  end# of #transform_district_tables_to_hash
+  
+  describe "#print_hashes_as_json" do
+    it 'saves hashes to a file' do
+    
+    end
+  end# of #print_hashes_as_json
 end# of spec
